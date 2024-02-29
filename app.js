e1608d6344c53d79727905f478a2767f23bb2ab4
Chris K <133260386+chrisjk02@users.noreply.github.com>
2024-02-29 14:02:17 -0800

    Create function to display table view of library array to console

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
+function printMyLibrary() {
+    console.table(myLibrary);
+}
+
 const book1 = new Book("Mary HK Choi", "Yolk", "600", "no");
+const book2 = new Book("Rick Riordan", "Percy Jackson and the Olympians", "350", "yes");
 addBookToLibrary(book1);
-console.log(myLibrary);
+addBookToLibrary(book2);
+printMyLibrary();
+
