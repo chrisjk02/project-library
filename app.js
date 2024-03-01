0d040f783e612907111e0158dfd47e3a75670133
Chris K <133260386+chrisjk02@users.noreply.github.com>
2024-03-01 10:30:23 -0800

    Add delete icon when generating a new card to allow user to delete previous entries

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const newBook = document.querySelector("#newBook");
 const sectionLibrary = document.querySelector("#library");
+const deleteButton = document.querySelector(".deleteButton");
+const xmlns = "http://www.w3.org/2000/svg";
 
 function Book(author, title, pages, isRead) {
     this.author = author;
@@ -27,13 +29,19 @@ function generateCard(data) {
     const titleText = document.createElement('div');
     const pagesText = document.createElement('div');
     const isReadText = document.createElement('div');
+    const svgDelete = document.createElementNS(xmlns, "svg");
+    svgDelete.setAttributeNS(null, "viewBox", "0 0 448 512");
+    const path = document.createElementNS(xmlns, "path");
+    path.setAttributeNS(null, "d", "M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z");
+    svgDelete.appendChild(path);
+    svgDelete.classList.add("deleteButton");
 
     data.forEach(element => {
         authorText.textContent = element.author;
         titleText.textContent = element.title;
-        pagesText.textContent = element.pages;
+        pagesText.textContent = element.pages + " pages";
         isReadText.textContent = element.isRead;
-        newCard.append(authorText, titleText, pagesText, isReadText);
+        newCard.append(authorText, titleText, pagesText, isReadText, svgDelete);
     });
 
     sectionLibrary.appendChild(newCard);
