bf14bac951facde5c2d7e834e044bdf112a3062e
Chris K <133260386+chrisjk02@users.noreply.github.com>
2024-03-01 11:26:51 -0800

    Add data attribute of index to each new card's delete button to reference against myLibrary

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const newBook = document.querySelector("#newBook");
 const sectionLibrary = document.querySelector("#library");
-const deleteButton = document.querySelector(".deleteButton");
+
 const xmlns = "http://www.w3.org/2000/svg";
 
 function Book(author, title, pages, isRead) {
@@ -35,6 +35,7 @@ function generateCard(data) {
     const path = document.createElementNS(xmlns, "path");
     path.setAttributeNS(null, "d", "M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z");
     deleteBtn.classList.add("deleteButton");
+
     svgDelete.appendChild(path);
     deleteBtn.appendChild(svgDelete);
 
@@ -47,6 +48,11 @@ function generateCard(data) {
     });
 
     sectionLibrary.appendChild(newCard);
+
+    const items = document.querySelectorAll('.deleteButton');
+    myLibrary.forEach((item, index) => {
+        items[index].dataset.index = index;
+    });
 }
 
 //Show dialog
