20a9ef0eb384ce705a6dfea8a77ba4854ce2d6d1
Chris K <133260386+chrisjk02@users.noreply.github.com>
2024-03-01 10:39:18 -0800

    Change svg to a clickable button

--- app.js
+++ app.js
@@ -29,19 +29,21 @@ function generateCard(data) {
     const titleText = document.createElement('div');
     const pagesText = document.createElement('div');
     const isReadText = document.createElement('div');
+    const deleteBtn = document.createElement('button');
     const svgDelete = document.createElementNS(xmlns, "svg");
     svgDelete.setAttributeNS(null, "viewBox", "0 0 448 512");
     const path = document.createElementNS(xmlns, "path");
     path.setAttributeNS(null, "d", "M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z");
+    deleteBtn.classList.add("deleteButton");
     svgDelete.appendChild(path);
-    svgDelete.classList.add("deleteButton");
+    deleteBtn.appendChild(svgDelete);
 
     data.forEach(element => {
         authorText.textContent = element.author;
         titleText.textContent = element.title;
         pagesText.textContent = element.pages + " pages";
         isReadText.textContent = element.isRead;
-        newCard.append(authorText, titleText, pagesText, isReadText, svgDelete);
+        newCard.append(authorText, titleText, pagesText, isReadText, deleteBtn);
     });
 
     sectionLibrary.appendChild(newCard);
