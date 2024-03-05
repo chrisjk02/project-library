2502f86fa4cbb21018b72248fd750af4136cd9a3
Chris Kim <133260386+chrisjk02@users.noreply.github.com>
2024-03-04 16:28:54 -0800

    Change read status button from text to an icon

--- app.js
+++ app.js
@@ -40,13 +40,19 @@ function generateCard(data) {
 
         const cardIsRead = document.createElement('div');
         cardIsRead.textContent = book.isRead;
-        bookCard.appendChild(cardIsRead);
+        cardIsRead.classList.add("flex-item");
 
-        const cardChangeIsRead = document.createElement('button');
-        cardChangeIsRead.textContent = "Status";
+        const cardChangeIsRead = document.createElement('div');
+        if (myLibrary[index].isRead === "Finished") {
+            cardChangeIsRead.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>';
+        } else {
+            cardChangeIsRead.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>';
+        }
+        cardChangeIsRead.classList.add("read-status-btn");
         cardChangeIsRead.setAttribute("data-index", index);
         cardChangeIsRead.addEventListener("click", book.changeReadStatus);
-        bookCard.appendChild(cardChangeIsRead);
+        cardIsRead.appendChild(cardChangeIsRead);
+        bookCard.appendChild(cardIsRead);
 
         const removeButton = document.createElement('button');
         removeButton.textContent = "Remove";
